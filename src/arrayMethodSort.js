a945115f42692b27ccb7fc8dbf44944991f24642
dimon <80713na@gmail.com>
2025-01-25 00:45:09 +0200

    sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,27 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (
+    compareFunction = (a, b) => a.toString() > b.toString(),
+  ) {
+    let isSwapped;
+
+    for (let i = 0; i < this.length; i++) {
+      isSwapped = false;
+
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+          isSwapped = true;
+        }
+      }
+
+      if (!isSwapped) {
+        break;
+      }
+    }
+
+    return this;
   };
 }
 
